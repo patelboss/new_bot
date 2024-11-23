891c6bb7fa3aa4150c32f9d357787c0d6394044a
patelboss <83952959+patelboss@users.noreply.github.com>
2024-11-24 01:02:39 +0530

    👍🏻

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,13 +75,12 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-    
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             if REQUEST_TO_JOIN_MODE == True:
-                invite_link = await client.create_chat_invite_link(chat_id=(int(AUTH_CHANNEL)), creates_join_request=True)
+                invite_link = await client.create_chat_invite_link(chat_id=(int(AUTH_CHANNELS)), creates_join_request=True)
             else:
-                invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+                invite_link = await client.create_chat_invite_link(int(AUTH_CHANNELS))
         except ChatAdminRequired:
             await message.reply_text("Make sure Bot is admin in Forcesub channel")
             return
