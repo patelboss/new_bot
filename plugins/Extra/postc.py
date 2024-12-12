022af975cfc63b7074e159a50cbb82a604f885dc
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-13 02:26:05 +0530

    Update postc.py

--- plugins/Extra/postc.py
+++ plugins/Extra/postc.py
@@ -91,6 +91,93 @@ async def post_reply(client, message):
 #        logger.exception(f"Failed to post the message. Error: {str(e)}")
         await message.reply(f"Failed to post the message. Error: {str(e)}")
 
+@Client.on_message(filters.command("ppost"))
+async def post_reply(client, message):
+    command_parts = message.text.split()
+    if len(command_parts) < 2 or not message.reply_to_message:
+#        logger.warning("Command is missing required parts or no reply message found.")
+        await message.reply("Please provide a valid channel ID and reply to a message using /cpost <channel_id>.\nUse /chelp to know about formatting\nIf you Dont Know Your Channel Id\nJust Forward Me Any Message From Your Channel And Reply That Message /id .")
+        return
+
+    channel_id = command_parts[1]
+#    logger.info(f"Extracted channel_id: {channel_id}")
+
+    if not channel_id.startswith("-100"):
+#        logger.error(f"Invalid channel ID: {channel_id}")
+        await message.reply("Invalid channel ID. Please provide a valid channel ID starting with '-100'.")
+        return
+
+    # Check if the user is admin or owner of the target channel
+    user_id = message.from_user.id  # Get the user ID of the person issuing the command
+    if not await is_user_admin_or_owner(client, channel_id, user_id):
+        await message.reply("You don't have permission to post in this channel.")
+        return
+
+    replied_message = message.reply_to_message
+    caption = replied_message.caption or replied_message.text or ""
+ #   logger.info(f"Replied message caption: {caption}")
+
+    inline_buttons = extract_buttons_from_caption(caption)
+    caption_without_buttons = remove_button_links(caption)
+    reply_markup = InlineKeyboardMarkup(inline_buttons) if inline_buttons else None
+
+    try:
+        if replied_message.photo:
+#            logger.info("Replied message is a photo. Sending to the channel...")
+            await client.send_photo(
+                chat_id=channel_id,
+                photo=replied_message.photo.file_id,
+                caption=caption_without_buttons,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                protect_content=True
+            )
+        elif replied_message.video:
+#            logger.info("Replied message is a video. Sending to the channel...")
+            await client.send_video(
+                chat_id=channel_id,
+                video=replied_message.video.file_id,
+                caption=caption_without_buttons,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                protect_content=True
+            )
+        elif replied_message.document:
+#            logger.info("Replied message is a document. Sending to the channel...")
+            await client.send_document(
+                chat_id=channel_id,
+                document=replied_message.document.file_id,
+                caption=caption_without_buttons,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                protect_content=True
+            )
+        elif replied_message.text:
+#            logger.info("Replied message is text. Sending to the channel...")
+            await client.send_message(
+                chat_id=channel_id,
+                text=caption_without_buttons,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                protect_content=True
+            )
+        else:
+#            logger.error("Unsupported media type in the replied message.")
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="Unsupported media type to forward.",
+            )
+
+        await message.reply(f"Message posted to channel {channel_id} successfully!")
+#        logger.info(f"Message posted to channel {channel_id} successfully.")
+    except Exception as e:
+#        logger.exception(f"Failed to post the message. Error: {str(e)}")
+        await message.reply(f"Failed to post the message. Error: {str(e)}")
+
 
 async def is_user_admin_or_owner(client, channel_id, user_id):
     """
@@ -183,7 +270,9 @@ Simply type <code>&gt;</code> at the beginning of a line:
 <b>11. Inline Buttons:</b>
 <code>{BUTTON TEXT}-{URL}</code>
 <i>Note:</i> Don't Use <b>Wrong URL</b> &amp; Don't Add <b>Extra Spaces</b> 
-
-Join @Filmykeedha For More Updates.
+<b> Use Command 
+<b> /cpost to post in channel & </b>
+<b> /ppost to post private post (that can't be forwarded) </b>
+<b> Join @Filmykeedha For More Updates.</b>
 """
     await message.reply(help_text, parse_mode=ParseMode.HTML)
