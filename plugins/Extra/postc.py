85e28d177f267985f62bd3f1825ebb3f03bd3314
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-13 04:56:05 +0530

    Update postc.py

--- plugins/Extra/postc.py
+++ plugins/Extra/postc.py
@@ -19,7 +19,7 @@ sticker_ids = [
 def get_random_sticker():
     return random.choice(sticker_ids)
 
-@Client.on_message(filters.command("ppost"))
+@Client.on_message(filters.command("cpost"))
 async def post_reply(client, message):
     random_sticker = get_random_sticker()
     m = await message.reply_sticker(random_sticker)
