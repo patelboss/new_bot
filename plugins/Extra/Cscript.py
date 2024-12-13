f4d97f06b3602c1b1b3d6ef9a6a1cb3697a37a0d
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-13 08:06:30 +0530

    Update and rename Cscript to Cscript.py

--- plugins/Extra/Cscript.py
+++ plugins/Extra/Cscript.py
@@ -19,4 +19,56 @@ TEXTS = {
         "PeerIdInvalid": "The bot's access to the channel is invalid. Please add the bot to the group again. If the bot is already in the group, try removing and re-adding it.",
         "ChatAdminRequired": "The bot needs to be an admin in the group & channel to perform this action."
     }
+
+    "HELP_TEXT": """
+    <b>Telegram Markdown & Formatting Guide:</b>
+
+    <i>You can use these formatting methods in your messages:</i>
+
+    <b>1. Bold:</b>
+    <code>*Your Text*</code> → <b>Your Text</b>
+
+    <b>2. Italic:</b>
+    <code>_Your Text_</code> → <i>Your Text</i>
+
+    <b>3. Underline:</b>
+    <code>__Your Text__</code> → <u>Your Text</u>
+
+    <b>4. Strikethrough:</b>
+    <code>~Your Text~</code> → <s>Your Text</s>
+
+    <b>5. Monospace:</b>
+    <code>`Your Text`</code> → <code>Your Text</code>
+
+    <b>6. Preformatted Block:</b>
+    <code>```Your Text```</code> → 
+    <pre>Your Text</pre>
+
+    <b>7. Spoiler:</b>
+    <code>||Your Text||</code> → <tg-spoiler>Your Text</tg-spoiler>
+
+    <b>8. Quote:</b>
+    Simply type <code>&gt;</code> at the beginning of a line:
+    <code>&gt; Your Text</code> → 
+    <blockquote>Your Text</blockquote>
+
+    <b>9. Inline Link:</b>
+    <code>[Text](https://example.com)</code> → [Text](https://example.com)
+
+    <b>10. Custom Inline Code:</b>
+    <code>```
+    <code>Custom text or programming code</code>
+    ```</code> → Displays a block of code.
+
+    <b>11. Inline Buttons:</b>
+    <code>{BUTTON TEXT}-{URL}</code>
+    <i>Note:</i> Don't Use <b>Wrong URL</b> &amp; Don't Add <b>Extra Spaces</b> 
+    <b> Use Command 
+    <b> /cpost to post in channel & </b>
+    <b> /ppost to post private post (that can't be forwarded) </b>
+    <b> Join @Filmykeedha For More Updates.</b>
+    """,
+    "STICKER_ID": "CAACAgIAAxkBAAIMU2dBzBWjzGCg_x2tFumZ76z5l5JiAAJiAANOXNIpTqLDGEjEK3EeBA"
+
+
 }
