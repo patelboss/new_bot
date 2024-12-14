98d615f57ee30234d220ffd744e330b9036a77fc
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-15 04:12:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -444,7 +444,7 @@ async def start(client, message):
             file_id = file["file_id"]
             files_ = await get_file_details(file_id)
             files1 = files_
-            title = {clean_file_name(file['file_name'])}
+            title = {clean_file_name(files['file_name'])}
             size=get_size(files1["file_size"])
             f_caption=files1["caption"]
             if CUSTOM_FILE_CAPTION:
@@ -585,7 +585,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_
-    title = '@Filmykeedha  ' + {clean_file_name(file['file_name'])}
+    title = '@Filmykeedha  ' + {clean_file_name(files['file_name'])}
     size=get_size(files["file_size"])
     f_caption=files["caption"]
     if CUSTOM_FILE_CAPTION:
