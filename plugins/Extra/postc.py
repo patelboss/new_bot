ae7a8afc23ff26f2ffbcc1cd021a4da2eea7b6e5
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-18 03:09:58 +0530

    Update postc.py

--- plugins/Extra/postc.py
+++ plugins/Extra/postc.py
@@ -139,33 +139,6 @@ def remove_button_links(caption: str):
 def get_random_sticker():
     return random.choice(TEXTS["random_sticker"])
 
-@Client.on_message(filters.command("chelp"))
-async def chelp(client, message):
-    # Check if the user added "m" after the command
-    command_parts = message.text.split()
-    use_html = len(command_parts) > 1 and command_parts[1].lower() == "m"
-
-    # Send the sticker first
-    sticker_id = get_random_sticker()
-    
-    m = await message.reply_sticker(sticker_id, 'CAACAgIAAxkBAAEWouFnYZdWgByiIdBga-j3bXRMK7sL3QACYgADTlzSKU6iwxhIxCtxHgQ')  # Default sticker ID if missing
-    await asyncio.sleep(2)  # Wait for 2 seconds
-    await m.delete()  # Delete the sticker message
-
-    if use_html:
-        # Log and send the help text in HTML format
-      #  client.LOGGER(__name__).info("Sending help text in HTML format.")
-        await message.reply(
-            TEXTS.get("HELP_TEXT", "<b>Help text not available.</b>"),
-            parse_mode=ParseMode.HTML
-        )
-    else:
-        # Log and display available text methods in Markdown format
-        #client.LOGGER(__name__).info("Sending available text methods in Markdown format.")
-        await message.reply(
-            TEXTS.get("AVAILABLE_TEXT_METHODS", "**Available methods not found.**"),
-            parse_mode=ParseMode.MARKDOWN
-        )        
 async def is_user_admin_or_owner(client, chat_id, user_id):
     """
     Check if the given user is an admin or the owner of a chat (channel or group).
